0f5cef6e33665598c36c607bea0e7e05344d39ca
Samuelec14 <samuconf14@gmail.com>
2023-10-10 11:06:43 +0200

    order model config

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function dishes(){
+        return $this->belongsToMany(Dish::class);
+    }
+
+    protected $fillable=[
+        'total_price',
+        'address',
+        'address_number',
+        'user_id',
+    ];
 }
