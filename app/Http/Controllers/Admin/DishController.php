6aede05a43e82039ee78b301f86f30f976f4580f
MembriniLuca29 <lucamembrini29@gmail.com>
2023-10-10 17:15:49 +0200

    controller in admin

--- app/Http/Controllers/Admin/DishController.php
+++ app/Http/Controllers/Admin/DishController.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
-use App\Models\Type;
-use Illuminate\Http\Request;
+use App\Models\Dish;
+use App\Http\Requests\StoreDishRequest;
+use App\Http\Requests\UpdateDishRequest;
 
-class TypeController extends Controller
+class DishController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -26,7 +27,7 @@ class TypeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreDishRequest $request)
     {
         //
     }
@@ -34,7 +35,7 @@ class TypeController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Type $type)
+    public function show(Dish $dish)
     {
         //
     }
@@ -42,7 +43,7 @@ class TypeController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Type $type)
+    public function edit(Dish $dish)
     {
         //
     }
@@ -50,7 +51,7 @@ class TypeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Type $type)
+    public function update(UpdateDishRequest $request, Dish $dish)
     {
         //
     }
@@ -58,7 +59,7 @@ class TypeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Type $type)
+    public function destroy(Dish $dish)
     {
         //
     }
