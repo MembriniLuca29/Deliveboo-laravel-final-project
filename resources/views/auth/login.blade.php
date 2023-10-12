a223665ddd9dc8edae620fb61a432f22d2397905
Samuelec14 <samuconf14@gmail.com>
2023-10-12 15:34:20 +0200

    unstyled fuctioning login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,7 +58,7 @@
         <!-- Scripts -->
         @vite('resources/js/app.js')
     </head>
-    <body>
+    <body class="my-style-login">
         <header>
 
                     {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -88,14 +88,26 @@
                         {{-- </ul>  --}}
                         
                         <nav class="d-flex justify-content-center align-items-center w-100">
-                            {{-- immagine logo  --}}
+                            
                             <div class="mx-3">
-                                LOGO
+                                {{-- logo  --}}
+                                <img src="images/iconlogo.png" alt="Logo">
                             </div>
-                            <h1 class="mx-3 fs-1">
-                                
+                                {{-- title  --}}
+                            <h1 class="mx-3 fw-bold">
+                                D E L I V E B O O
                             </h1>
                         </nav>
+
+                        @auth
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <button type="submit" class="btn btn-outline-danger">
+                                    Log Out
+                                </button>
+                            </form>
+                        @endauth
 
                         @auth
                             <form method="POST" action="{{ route('logout') }}">
@@ -112,7 +124,87 @@
 
         <main class="py-4">
 
-            <form method="POST" action="{{ route('login') }}">
+            <div class="container h-100">
+                <div class=" h-100 w-100 d-flex justify-content-center align-items-center">
+                    <div class="my-card bg-white py-4 rounded" style="">
+
+                        {{-- Form di accesso  --}}
+                        <div id="registrer-form" class="box ms-5 me-5 text-center">
+                            <h3 class="fs-4 text-uppercase mt-1 mb-4">accesso utente</h3>
+
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
+                                    <label for="email">Indirizzo Email</label>
+                                </div>
+
+                                <div class="form-floating mb-3">
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                                    <label for="password">Password</label>
+                                </div>
+
+                                <div class="mt-4"><small><a href="{{ route('register') }}">Non hai un account? Registrati</a></small></div>
+                                <button id="btn-1" type="submit" class="btn btn-warning px-4 my-1 mt-2">
+                                    <strong>
+                                        Accedi
+                                    </strong>
+                                </button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+                {{-- <form method="POST" action="{{ route('register') }}">
+                    @csrf
+            
+                    <!-- Name -->
+                    <div>
+                        <label for="name">
+                            Name
+                        </label>
+                        <input type="text" id="name" name="name">
+                    </div>
+            
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <label for="email">
+                            Email
+                        </label>
+                        <input type="email" id="email" name="email">
+                    </div>
+            
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <label for="password">
+                            Password
+                        </label>
+                        <input type="password" id="password" name="password">
+                    </div>
+            
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <label for="password_confirmation">
+                            Conferma Password
+                        </label>
+                        <input type="password" id="password_confirmation" name="password_confirmation">
+                    </div>
+            
+                    <div>
+                        <a href="{{ route('login') }}">
+                            {{ __('Already registered?') }}
+                        </a>
+            
+                        <button type="submit">
+                            Register
+                        </button>
+                    </div>
+                </form>  --}}
+            </div>
+
+
+            {{-- <form method="POST" action="{{ route('login') }}">
                 @csrf
         
                 <!-- Email Address -->
@@ -150,7 +242,7 @@
                         Log in
                     </button>
                 </div>
-            </form>
+            </form> --}}
             <h1>
                 {{ $errors->first('email') }}
             </h1>
