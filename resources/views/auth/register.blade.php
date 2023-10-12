b80ca63b27dda0028d7fe2aa95fd194045f1ce81
Samuelec14 <samuconf14@gmail.com>
2023-10-12 16:26:16 +0200

    completed and commented register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,34 +11,8 @@
         @vite('resources/js/app.js')
     </head>
     <body class="my-style-register">
-        <header>
-
-                    {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button> --}}
-                    {{-- <div class="" id="navbarText"> --}}
-                        {{-- <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100">
-                            @auth
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link 2</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link 3</a>
-                                </li>
-                            @else
-                                {{-- <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">login</a>
-                                </li> --}}
-                                
-                            {{-- @endauth --}}
-                        {{-- </ul>  --}}
-                        
+        
+        <header>        
                         <nav class="d-flex justify-content-center align-items-center w-100">
                             {{-- immagine logo  --}}
                             <div class="mx-3">
@@ -58,8 +32,6 @@
                                 </button>
                             </form>
                         @endauth
-                    {{-- </div> --}}
-
         </header>
 
         <main class="py-4">
@@ -72,36 +44,44 @@
                             <div class="col">
                                 {{-- Form di registrazione  --}}
                                 <div id="registrer-form" class="box ms-5 me-2 text-center">
-                                    <h3 class="fs-4 text-uppercase mt-1 mb-4">creazione utente</h3>
+                                    <h3 class="fs-4 mt-1 mb-4">Registrazione Utente</h3>
 
                                     <form method="POST" action="{{ route('register') }}">
                                         @csrf
 
+                                        {{-- name --}}
                                         <div class="form-floating mb-3">
                                             <input type="text" class="form-control" id="name" name="name" placeholder="name@example.com">
                                             <label for="name">Nome</label>
                                         </div>
 
+                                        {{-- email address --}}
                                         <div class="form-floating mb-3">
                                             <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
                                             <label for="email">Indirizzo Email</label>
                                         </div>
 
+                                        {{-- password --}}
                                         <div class="form-floating mb-3">
                                             <input type="password" class="form-control" id="password" name="password" placeholder="Password">
                                             <label for="password">Password</label>
                                         </div>
 
+                                        {{-- password_confirmation --}}
                                         <div class="form-floating mb-3">
                                             <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Conferma Password">
                                             <label for="password_confirmation">Conferma Password</label>
                                         </div>
-                                        <div class="mt-4"><small><a href="{{ route('login') }}">Sei già registrato? Accedi</a></small></div>
-                                        <button id="btn-1" type="submit" class="btn btn-warning px-4 my-1 mt-2">
+
+                                        {{-- submit button  --}}
+                                        <button id="btn-1" type="submit" class="btn btn-warning px-4 my-1 mt-4">
                                             <strong>
                                                 Registrati
                                             </strong>
                                         </button>
+                                        {{-- Already registered? --}}
+                                        <div class="mt-1"><small><a href="{{ route('login') }}">Sei già registrato? Accedi</a></small></div>
+
                                     </form>
                                 </div>
                             </div>
@@ -111,9 +91,9 @@
                                 <div id="benefits-info" class="box ms-2 me-5 mt-2">
                                     <div class="infobox ms-3 mt-4 d-xxl-block">
                                         <h3 class="fs-5 mb-3">Cosa vuol dire essere partner Deliveboo?</h4>
-                                        <div class="mb-3 ms-2">"Espandi la tua attività grazie a Deliveboo, raggiungendo nuovi clienti ogni giorno."</div>
-                                        <div class="mb-3 ms-2">"Un modo semplice ed efficacie per gestire le ordinazioni ed avere tutto sotto controllo."</div>
-                                        <div class="mb-3 ms-2 d-none d-lg-block">"Con Deliveboo vedrai aumentare la visibilità online del tuo ristorante come mai prima d'ora."</div>
+                                        <div class="mb-3 ms-2">"Con Deliveboo espandi la tua attività, raggiungendo nuovi clienti ogni giorno."</div>
+                                        <div class="mb-3 ms-2">"È un modo semplice ed efficacie per gestire le ordinazioni ed avere tutto sotto controllo."</div>
+                                        <div class="mb-3 ms-2 d-none d-lg-block">"Da Partner vedrai aumentare la visibilità online del tuo ristorante come mai prima d'ora."</div>
                                         <div class="mb-3 ms-2 d-none d-xxl-block">"Deliveboo ha il sistema di consegne più ottimizzato sul mercato, i tuoi clienti saranno sempre soddisfatti."</div>
                                     </div>
                                 </div>
