d8a308f890e1e0f58f2a50a4916fd966443ef9da
Samuelec14 <samuconf14@gmail.com>
2023-10-12 16:29:05 +0200

    .

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -102,54 +102,54 @@
                         </div>
                     </div>
                 </div>
-                {{-- <form method="POST" action="{{ route('register') }}">
-                    @csrf
-            
-                    <!-- Name -->
-                    <div>
-                        <label for="name">
-                            Name
-                        </label>
-                        <input type="text" id="name" name="name">
-                    </div>
-            
-                    <!-- Email Address -->
-                    <div class="mt-4">
-                        <label for="email">
-                            Email
-                        </label>
-                        <input type="email" id="email" name="email">
-                    </div>
-            
-                    <!-- Password -->
-                    <div class="mt-4">
-                        <label for="password">
-                            Password
-                        </label>
-                        <input type="password" id="password" name="password">
-                    </div>
-            
-                    <!-- Confirm Password -->
-                    <div class="mt-4">
-                        <label for="password_confirmation">
-                            Conferma Password
-                        </label>
-                        <input type="password" id="password_confirmation" name="password_confirmation">
-                    </div>
-            
-                    <div>
-                        <a href="{{ route('login') }}">
-                            {{ __('Already registered?') }}
-                        </a>
-            
-                        <button type="submit">
-                            Register
-                        </button>
-                    </div>
-                </form>  --}}
             </div>
-
+            
         </main>
     </body>
-</html>
+    </html>
+    {{-- <form method="POST" action="{{ route('register') }}">
+        @csrf
 
+        <!-- Name -->
+        <div>
+            <label for="name">
+                Name
+            </label>
+            <input type="text" id="name" name="name">
+        </div>
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <label for="email">
+                Email
+            </label>
+            <input type="email" id="email" name="email">
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <label for="password">
+                Password
+            </label>
+            <input type="password" id="password" name="password">
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <label for="password_confirmation">
+                Conferma Password
+            </label>
+            <input type="password" id="password_confirmation" name="password_confirmation">
+        </div>
+
+        <div>
+            <a href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
+            <button type="submit">
+                Register
+            </button>
+        </div>
+    </form>  --}}
+    
