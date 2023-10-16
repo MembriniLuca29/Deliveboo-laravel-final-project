7cad4d6b56ff8ca5b95c293147c6627b0aa263ec
Samuelec14 <samuconf14@gmail.com>
2023-10-16 19:01:17 +0200

    dashboard home , missing items

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,8 +11,82 @@
 
 {{-- MAIN CONTENT SECTION  --}}
 @section('main-content')
-    <div class="my-style-home-dashboard-main">
+    <div class="my-style-home-dashboard-main  h-100">
 
+        <div class="px-5 h-100">
+            <div class="px-4  h-100">
+
+                {{-- intestazione main  --}}
+                <div class="header d-inline">
+                    <h1 class="lh-1 mb-0 fw-normal">Ciao {{ auth()->user()->name }},</h1>
+                    <span class="fs-3 border-bottom border-warning border-3">qui puoi gestire e monitorare i tuoi ristoranti!</span>
+                </div>
+
+                <div id="main-content" class="mt-5 h-100">
+
+                    {{-- main top content  --}}
+                    <div id="main-top-cont" class="pt-4 d-flex justify-content-between">
+                        <div class="fs-3">
+                            I Tuoi Ristoranti
+                        </div>
+
+                        {{-- bottone aggiunta ristorante  --}}
+                        <div class="add-button">
+                            <div type="submit" class="btn btn-1 btn-green px-3 mb-3 fw-semibold">
+                                    <a class="text-decoration-none" href="{{ route('restaurants.create') }}">+ Aggiungi</a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {{-- main bottom content  --}}
+                    <div id="main-bot-cont" class="w-100">
+                        <div class="px-4 pt-1 pb-3 h-100">
+                            <div class="h-100 border-start border-end border-warning border-3 px-5">
+                                <div class="mx-1 pt-1 h-100">
+
+                                    {{-- Restaurants searchbar  --}}
+                                    <form id="res-search" class="d-flex" role="search">
+                                        <input class="form-control border-1 border-dark py-2" type="search" placeholder="Cerca il ristorante di cui hai bisogno..." aria-label="Search">
+                                        {{-- <button class="btn btn-outline-success" type="submit">Search</button> --}}
+                                    </form>
+
+                                    {{-- restaurants list section --}}
+                                    <div id="res-list" class="pt-3 overflow-auto overflow-x-hidden px-2">
+
+                                        @foreach ($restaurants as $restaurant)
+                                        {{-- my single restaurant item  --}}
+                                        <div class="item bg-secondary mb-3 p-2 row">
+                                            <div class="col-4">
+                                                <div class="img-box bg-white rounded h-100">
+                                                    <img src="{{ asset('storage/'.$restaurant->thumb) }}" alt="">
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                                <div class="text-center">
+                                                    {{ $restaurant->name }}
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                                <div class="text-center">
+                                                    Bottoni interazioni
+                                                </div>
+                                            </div>
+                                        </div> 
+                                        @endforeach
+                                        
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
         {{-- <div class="row">
             <h1 class="text-center text-success">
                 Bentornato 
