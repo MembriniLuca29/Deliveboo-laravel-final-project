3c37fc1b4c9a7ab56c8dbe6d0c741e31e902639c
MembriniLuca29 <lucamembrini29@gmail.com>
2023-10-17 09:48:58 +0200

    home dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,11 @@
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('dashboard') }}">
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://localhost:5174/">
                                     Home
                                 </a>
                             </li>
