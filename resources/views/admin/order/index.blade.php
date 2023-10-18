97b8a33b355fda5463ffd301861334c750120429
MembriniLuca29 <lucamembrini29@gmail.com>
2023-10-18 16:48:08 +0200

    card order

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -7,9 +7,18 @@
 
     @foreach ($orders as $orderArray)
         @foreach ($orderArray as $order)
-            <div>
-                {{ $order->name }}
-            </div>
+           
+
+            <div class="card order-card ms-4 mb-2" >
+                <div class="card-header">
+                    {{ $order->name }} {{ $order->last_name }} <h6>data: {{ $order->updated_at }} </h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">numero: {{ $order->phone_number }} </li>
+                  <li class="list-group-item">email: {{ $order->email }}</li>
+                  <li class="list-group-item">stato: {{ $order->status}}</li>
+                </ul>
+              </div>
         @endforeach
     @endforeach
 
