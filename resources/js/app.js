d8d0080d9affcedd8d4a6c24b266b4b4cf276c5f
Samuelec14 <samuconf14@gmail.com>
2023-10-25 11:59:25 +0200

    dishes filtering working

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,8 +8,22 @@ import.meta.glob([
     '../img/**'
 ]);
 
-
-
+// FUNCTION TO FILTER DAHSBOARD SEARCHBAR
+function FilterFunction(){
+    document.getElementById("search-input").addEventListener("input", function() {
+        var searchTerm = this.value.toLowerCase();
+        var items = document.getElementsId("single-item");
+    
+        for (var i = 0; i < items.length; i++) {
+            var itemName = items[i].getElementsById("item-name")[0].innerText.toLowerCase();
+            if (itemName.includes(searchTerm)) {
+                items[i].style.display = "block";
+            } else {
+                items[i].style.display = "none";
+            }
+        }
+    });
+}
 
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,6 +109,22 @@
                 });
             });
 
+            // Funzione filtraggio pietanze per nome 
+            document.getElementById("search-input").addEventListener("input", function() {
+                var searchTerm = this.value.toLowerCase();
+                var items = document.getElementsByClassName("bg-secondary");
+                console.log(items)
+
+                for (var i = 0; i < items.length; i++) {
+                    var itemName = items[i].querySelector(".dish-name").innerText.toLowerCase();
+                    if (itemName.includes(searchTerm)) {
+                        items[i].style.display = "flex";
+                    } else {
+                        items[i].style.display = "none";
+                    }
+                }
+            });
+
         </script>
     </body>
 </html>
