c2361028a53a0ecd42c7085f428a0eed76104e44
MembriniLuca29 <lucamembrini29@gmail.com>
2023-10-27 10:37:49 +0200

    year work

--- resources/views/admin/order/stats.blade.php
+++ resources/views/admin/order/stats.blade.php
@@ -49,100 +49,68 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> 
 <script>
-   
-    // Recupera i timestamp degli ordini dalla variabile passata dal controller
-    const orders = @json($orders);
-    const timestamps = @json($timestamps);
-console.log(orders);
-    // Ora puoi utilizzare l'array timestamps nel tuo script JavaScript
-    const monthNames = [
-        'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno',
-        'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'
-    ];
+const orders = @json($orders);
+const timestamps = @json($timestamps);
+const monthNames = [
+    'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno',
+    'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'
+];
 
-    // FUNCTIONS
-function calculateMonthlyOrderCounts() {
+function calculateMonthlyOrderCounts(selectedYear) {
     const monthlyOrderCounts = {};
 
     // Itera attraverso gli ordini e aggiorna il conteggio per ogni mese
     orders.forEach(order => {
-        const month = new Date(order.updated_at).getMonth(); // Ottieni il numero del mese (da 0 a 11)
-        monthlyOrderCounts[month] = (monthlyOrderCounts[month] || 0) + 1; // Aggiorna il conteggio per il mese corrente
+        const orderYear = new Date(order.updated_at).getFullYear();
+        const orderMonth = new Date(order.updated_at).getMonth();
+        if (selectedYear === 0 || orderYear === selectedYear) {
+            monthlyOrderCounts[orderMonth] = (monthlyOrderCounts[orderMonth] || 0) + 1;
+        }
     });
 
     return monthlyOrderCounts;
 }
-// AFTER ORDERS DATA HAS BEEN OBTAINED
 
-// Creazione di un oggetto per conteggiare gli ordini per ogni mese
-const monthlyOrderCounts = calculateMonthlyOrderCounts();
-
-// Ottieni i dati per il grafico
-const data = Object.values(monthlyOrderCounts); // Quantità di ordini per ogni mese
-const labels = Object.keys(monthlyOrderCounts).map(month => monthNames[month]); // Nomi dei mesi
-
-// Aggiorna il grafico con i nuovi dati
-updateChart(labels, data);
+function calculateBackgroundColor(value) {
+    if (value < 5) {
+        return 'rgba(255, 99, 132, 0.8)';
+    } else if (value < 10) {
+        return 'rgba(255, 159, 64, 0.8)';
+    } else if (value < 20) {
+        return 'rgba(255, 205, 86, 0.8)';
+    } else {
+        return 'rgba(75, 192, 192, 0.8)';
+    }
+}
 
 const yearSelect = document.getElementById('year-select');
-yearSelect.addEventListener('change', () => {
-    const selectedYear = parseInt(yearSelect.value);
+if (yearSelect) {
+    let chartInstance;
+    
+    yearSelect.addEventListener('change', () => {
+        const selectedYear = parseInt(yearSelect.value);
+        const monthlyOrderCounts = calculateMonthlyOrderCounts(selectedYear);
+        const data = Object.values(monthlyOrderCounts);
+        const labels = Object.keys(monthlyOrderCounts).map(month => monthNames[month]);
 
-    // Aggiungi questa condizione per gestire l'opzione "Tutti gli anni"
-    const filteredOrders = selectedYear === 0 
-        ? orders 
-        : orders.filter(order => order.year === selectedYear);
-
-    const monthlyOrderCounts = calculateMonthlyOrderCounts(filteredOrders);
-    const data = Object.values(monthlyOrderCounts);
-    const labels = Object.keys(monthlyOrderCounts).map(month => monthNames[month]);
-
-    updateChart(labels, data);
-});
-
-function populateYearSelect(uniqueYears) {
-    const allYearsOption = document.createElement('option');
-    allYearsOption.value = 0;
-    allYearsOption.text = 'Tutti gli anni';
-    yearSelect.appendChild(allYearsOption);
-
-    uniqueYears.forEach(year => {
-        const option = document.createElement('option');
-        option.value = year;
-        option.text = year;
-        yearSelect.appendChild(option);
-    });
-}
-function updateChart(labels, data) {
-    // Aggiorna il grafico con i nuovi dati
-    const ctx = document.getElementById('myChart').getContext('2d');
-    // ... Resto del codice per creare il grafico ...
-}
-document.addEventListener('DOMContentLoaded', () => {
-    // Fetch names for labels (se necessario)
-
-    // CHART CREATION
-    const ctx = document.getElementById('myChart').getContext('2d');
-
-    // Calcola i dati e le etichette per il grafico
-    const monthlyOrderCounts = calculateMonthlyOrderCounts(); // Assumi che questa funzione restituisca i dati degli ordini per ogni mese
-    const data = Object.values(monthlyOrderCounts);
-    const labels = Object.keys(monthlyOrderCounts).map(month => monthNames[month]);
-
-    // Calcola i colori di sfondo delle barre
-    const backgroundColors = data.map(value => {
-        if (value < 5 || (value > 20 && value <= 49)) {
-            return 'rgba(255, 99, 132, 0.8)';
-        } else if ((value >= 5 && value < 10) || (value > 50 && value <= 99)) {
-            return 'rgba(39, 245, 153, 0.8)';
-        } else if (value >= 10 && value < 20) {
-            return 'rgba(39, 136, 245, 0.8)';
-        }
-        // Handle other cases if necessary
-        return 'rgba(0, 0, 0, 0.8)'; // Default color
+        const backgroundColors = data.map(value => calculateBackgroundColor(value));
+        
+        // Aggiorna solo i dati del grafico invece di crearne uno nuovo
+        chartInstance.data.labels = labels;
+        chartInstance.data.datasets[0].data = data;
+        chartInstance.data.datasets[0].backgroundColor = backgroundColors;
+        chartInstance.update(); // Aggiorna il grafico con i nuovi dati
     });
 
-    new Chart(ctx, {
+    // Inizializza il grafico con l'anno corrente come predefinito
+    const currentYear = new Date().getFullYear();
+    const monthlyOrderCounts = calculateMonthlyOrderCounts(currentYear);
+    const data = Object.values(monthlyOrderCounts);
+    const labels = Object.keys(monthlyOrderCounts).map(month => monthNames[month]);
+    const backgroundColors = data.map(value => calculateBackgroundColor(value));
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    chartInstance = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: labels,
@@ -166,11 +134,14 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     });
-});
+} else {
+    console.error('Elemento year-select non trovato nel DOM.');
+}
 
 </script>
 
 @endsection
+
 
 
 
