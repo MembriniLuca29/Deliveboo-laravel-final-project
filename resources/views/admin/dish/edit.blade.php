1de8d6261fa45ccd453602fd2c9cdf33ec7721d6
MembriniLuca29 <lucamembrini29@gmail.com>
2023-10-30 16:38:25 +0100

    .

--- resources/views/admin/dish/edit.blade.php
+++ resources/views/admin/dish/edit.blade.php
@@ -23,7 +23,7 @@
                     {{-- intestazione main --}}
                     <div id="main-top-cont" class="py-2 pb-3 d-flex justify-content-between">
                         <div class="fs-2 d-none d-md-inline">
-                            Aggiungi una pietanza
+                            Modifica la pietanza
                         </div>
                         <div class="fs-4 d-md-none">
                             Aggiungi una pietanza
