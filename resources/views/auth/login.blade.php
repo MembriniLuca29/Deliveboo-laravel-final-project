dde50767c483116399012df1f69af1e373319480
Samuelec14 <samuconf14@gmail.com>
2023-10-31 09:00:51 +0100

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,9 +39,7 @@
                                 <div class="form-floating mb-3">
                                     <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="name@example.com" value="{{ old('email') }}" required>
                                     <label for="email">Indirizzo email
-                                        <span class="text-danger">
-                                            *
-                                        </span>
+
                                     </label>
                                     @error('email')
                                         <div class="invalid-feedback">{{ $message }}</div>
@@ -51,9 +49,7 @@
                                 <div class="form-floating mb-1">
                                     <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" placeholder="Password" required>
                                     <label for="password">Password
-                                        <span class="text-danger">
-                                            *
-                                        </span>
+
                                     </label>
                                     
                                     @error('password')
